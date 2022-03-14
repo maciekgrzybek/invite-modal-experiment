6107ee6d36d742d070b5b89dffd737587bd5b571
Maciej Grzybek <maciekgrzybek1@gmail.com>
2022-03-14 18:04:25 +0100

    feat: some initial stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Center, ChakraProvider, theme, useDisclosure } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
+import { Popin } from './components/Popin';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure({ id: 'popin-modal' });
+  return (
+    <ChakraProvider theme={theme}>
+      <Center h="100vh">
+        <Button onClick={onOpen}>Invite teammates</Button>
+        <Popin isOpen={isOpen} onClose={onClose} />
+      </Center>
+    </ChakraProvider>
+  );
+};
